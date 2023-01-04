5356670054ba02f7bd2f60ae580cb0d8d17d07ba
mituya <atiutgh@gmail.com>
2023-01-04 10:03:13 +0900

    add:daysコントローラー作成

---
+++ app/controllers/days_controller.rb
@@ -0,0 +1,19 @@
+class DaysController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+  devise_for :users, controllers: {
+    # ↓ローカルに追加されたコントローラーを参照する(コントローラー名: "コントローラーの参照先")
+    registrations: "users/registrations",
+    sessions: "users/sessions"
+  }
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :days
 end
